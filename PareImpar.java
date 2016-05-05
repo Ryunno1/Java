b40e9f0c9d9de1f201bf62a03ed992970756b575
Rochester Rocha Gondim <rochagondim@hotmail.com>
2016-05-05 12:01:14 -0300

    Added files via upload

---
+++ PareImpar.java
@@ -0,0 +1,55 @@
+package prova;
+
+import javax.swing.JOptionPane;
+
+public class PareImpar {
+	
+		int limiteLinhas;
+		int opcao;
+		
+		void escolheTipoImpressao(){
+			opcao =Integer.parseInt( JOptionPane.showInputDialog("\nEscolha uma opção de impressão: "
+					+ "\n1: a partir de um número impar"
+					+ "\n2: a partir de um número par"
+					+ "\n3: todos os números"));
+		
+			
+			
+			limiteLinhas=Integer.parseInt(JOptionPane.showInputDialog("Escolha o limite de impressão de linhas"));
+		
+		}
+		
+		void imprimeTabela() {
+			for(int i = 1; i <= this.limiteLinhas; i++) {
+				for(int j = 1; j<=i; j++) {
+					int mult = i*j;
+					
+					if(this.opcao == 1) {
+						if(i%2 != 0) {
+							System.out.print(" "+mult);
+						}
+					}
+					if(this.opcao == 2) {
+						if(i%2 == 0) {
+							System.out.print(" "+mult);
+						}
+					}
+					if(this.opcao == 3) {
+						System.out.print(" "+mult);
+					}
+				}
+				System.out.println("");
+			}
+		}
+		
+		public static void main(String[] args) {
+			PareImpar pi = new PareImpar();
+			
+			pi.escolheTipoImpressao();
+			pi.imprimeTabela();
+			
+		
+		}
+	
+
+}
